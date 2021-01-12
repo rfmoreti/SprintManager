907f896b2896a2284e1742ba15cdd3cca66452c4
rfmoreti <rfmore.ti@gmail.com>
2021-01-11 21:39:20 -0300

    Projeto Model

---
+++ SprintManager_Model/Sprint_Model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SprintManager_Model
+{
+    public class Sprint_Model
+    {
+        public string Name { get; set; }
+        public DateTime FirstDay { get; set; }
+        public DateTime LastDay { get; set; }
+    }
+}
